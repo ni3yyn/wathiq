bcbb6d966acc493e33aadf9ba647abd75acfdfc8
ni3yyn <ni3yyn@gmail.com>
2025-11-29 15:56:10 +0100

    added fixed landingpage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', 
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  },
   build: {
     minify: 'esbuild',
     sourcemap: true, // Helps debugging (keeps code readable in DevTools)
